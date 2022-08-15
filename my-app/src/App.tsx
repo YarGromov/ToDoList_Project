22150dbe0d41b824f66098a650ec45b05eb4a94a
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-08-15 20:16:05 +0200

    useState(filteredTasks) 3

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,14 +4,12 @@ import {TasksPropsType, TodoList} from "./TodoList";
 
 function App() {
 
-    let initTasks: Array<TasksPropsType> = [
+    let [tasks, setTasks] = useState([
         {id: 1, title: 'HTML&CSS', isDone: true},
         {id: 2, title: 'JS', isDone: true},
         {id: 3, title: 'ReactJS', isDone: false},
         {id: 4, title: 'Redux', isDone: false}
-    ]
-
-    let [tasks, setTasks] = useState(initTasks)
+    ])
 
 
     const removeTask = (id: number) => {
