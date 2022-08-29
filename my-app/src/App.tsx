4ac51673f3c5af4e64f1cdf63f9e5d862a03a890
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-08-29 19:13:04 +0200

    Associative array

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -13,70 +13,81 @@ type TodolistType = {
 
 function App() {
 
-    let [tasks, setTasks] = useState([
-        {id: v1(), title: "HTML&CSS", isDone: true},
-        {id: v1(), title: "JS", isDone: true},
-        {id: v1(), title: "ReactJS", isDone: false},
-        {id: v1(), title: "Rest API", isDone: false},
-        {id: v1(), title: "GraphQL", isDone: false},
-    ]);
 
-
-
-    function removeTask(id: string) {
+    function removeTask(id: string, todolistId: string) {
+        let tasks = tasksObj[todolistId];
         let filteredTasks = tasks.filter(t => t.id != id);
-        setTasks(filteredTasks);
+        tasksObj[todolistId] = filteredTasks;
+        setTasks({...tasksObj});
     }
 
-    function addTask(title: string) {
+    function addTask(title: string, todolistId: string) {
         let task = {id: v1(), title: title, isDone: false};
+        let tasks = tasksObj[todolistId];
         let newTasks = [task, ...tasks];
-        setTasks(newTasks);
+        tasksObj[todolistId] = newTasks;
+        setTasks({...tasksObj});
     }
 
-    function changeStatus(taskId: string, isDone: boolean) {
+    function changeStatus(taskId: string, isDone: boolean, todolistId: string) {
+        let tasks = tasksObj[todolistId];
         let task = tasks.find(t => t.id === taskId);
         if (task) {
             task.isDone = isDone;
+            setTasks({...tasksObj});
         }
-
-        setTasks([...tasks]);
     }
 
 
-
-
     function changeFilter(value: FilterValuesType, todolistId: string) {
-        let todolist = todolists.find(tl=> tl.id === todolistId);
-        if(todolist){
+        let todolist = todolists.find(tl => tl.id === todolistId);
+        if (todolist) {
             todolist.filter = value;
             setTodolist([...todolists])
         }
     }
 
+    let todolistId1 = v1();
+    let todolistId2 = v1();
+
     let [todolists, setTodolist] = useState<Array<TodolistType>>([
-        {id: v1(), title: 'What to learn', filter: 'active'},
-        {id: v1(), title: 'What to buy', filter: 'completed'}
+        {id: todolistId1, title: 'What to learn', filter: 'active'},
+        {id: todolistId2, title: 'What to buy', filter: 'completed'}
     ]);
+
+
+    let [tasksObj, setTasks] = useState({
+        [todolistId1]: [
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "ReactJS", isDone: false},
+            {id: v1(), title: "Rest API", isDone: false},
+            {id: v1(), title: "GraphQL", isDone: false},
+        ],
+        [todolistId2]: [
+            {id: v1(), title: "Book", isDone: false},
+            {id: v1(), title: "Milk", isDone: true},
+        ]
+    })
 
     return (
         <div className="App">
             {
-                todolists.map((tl)=>{
+                todolists.map((tl) => {
 
-                    let tasksForTodolist = tasks;
+                    let tasksForTodolist = tasksObj[tl.id];
 
                     if (tl.filter === "active") {
-                        tasksForTodolist = tasks.filter(t => t.isDone === false);
+                        tasksForTodolist = tasksForTodolist.filter(t => t.isDone === false);
                     }
                     if (tl.filter === "completed") {
-                        tasksForTodolist = tasks.filter(t => t.isDone === true);
+                        tasksForTodolist = tasksForTodolist.filter(t => t.isDone === true);
                     }
 
                     return <Todolist title={tl.title}
                                      key={tl.id}
                                      id={tl.id}
-                                      tasks={tasksForTodolist}
+                                     tasks={tasksForTodolist}
                                      removeTask={removeTask}
                                      changeFilter={changeFilter}
                                      addTask={addTask}
