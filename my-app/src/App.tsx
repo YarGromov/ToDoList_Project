7b1f3683a9eb907219fd3fe6359b9cacadf07f52
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-10-30 12:25:27 +0100

    tasks1, tasks2

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,11 +3,22 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
+
+    const tasks1 = [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 1, title: 'JS', isDone: true},
+        {id: 1, title: 'React', isDone: false}
+    ]
+    const tasks2 = [
+        {id: 1, title: 'Hello World!', isDone: true},
+        {id: 1, title: 'I am happy!', isDone: true},
+        {id: 1, title: 'Yo!', isDone: false}
+    ]
+
     return (
         <div className="App">
-           <Todolist title={'What to learn'}/>
-           <Todolist title={'What to buy'}/>
-           <Todolist title={'What to read'}/>
+           <Todolist title={'What to learn'} tasks={tasks1}/>
+           <Todolist title={'What to buy'} tasks={tasks2}/>
         </div>
     );
 }
