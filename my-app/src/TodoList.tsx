fcf4553f28fe86489efd02f2ac35b0a9530c2484
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-10-30 14:37:21 +0100

    tasks.map

--- my-app/src/TodoList.tsx
+++ my-app/src/TodoList.tsx
@@ -1,34 +1,42 @@
-import React from "react";
+import React from 'react';
 
-export type TaskType = {
+type TaskType = {
     id: number
     title: string
     isDone: boolean
 }
 
-export type TodolistPropsType = {
+type PropsType = {
     title: string
     tasks: Array<TaskType>
 }
 
-export function Todolist(props: TodolistPropsType) {
-    return (
+export function Todolist(props: PropsType) {
+
+    const deleteButtonHandler = () =>{
+
+    }
+
+    return <div>
+        <h3>{props.title}<button onClick={deleteButtonHandler}>x</button></h3>
         <div>
-            <h3>{props.title}</h3>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            <ul>
-                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
-            </ul>
-            <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-            </div>
+            <input/>
+            <button>+</button>
         </div>
-    )
+        <ul>
+            {
+                props.tasks.map((tl) => {
+                        return (
+                            <li key={tl.id}><input type="checkbox" checked={tl.isDone}/> <span>{tl.title}</span></li>
+                        )
+                    }
+                )
+            }
+        </ul>
+        <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+        </div>
+    </div>
 }
