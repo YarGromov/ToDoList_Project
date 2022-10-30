ff8a53e1b9483ad885a8609227cadfb71025505c
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-10-30 12:51:40 +0100

    id

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,13 +6,13 @@ function App() {
 
     const tasks1 = [
         {id: 1, title: 'HTML&CSS', isDone: true},
-        {id: 1, title: 'JS', isDone: true},
-        {id: 1, title: 'React', isDone: false}
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false}
     ]
     const tasks2 = [
         {id: 1, title: 'Hello World!', isDone: true},
-        {id: 1, title: 'I am happy!', isDone: true},
-        {id: 1, title: 'Yo!', isDone: false}
+        {id: 2, title: 'I am happy!', isDone: true},
+        {id: 3, title: 'Yo!', isDone: false}
     ]
 
     return (
