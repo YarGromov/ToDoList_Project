079178a1f7a3dcfafda80f96477ceb73dcd7e010
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-11-14 19:47:19 +0100

    activateEditMode

--- my-app/src/EditableSpan.tsx
+++ my-app/src/EditableSpan.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 
 type EditableSpanPropsType = {
     title: string
@@ -8,11 +8,16 @@ type EditableSpanPropsType = {
 export function EditableSpan(props: EditableSpanPropsType) {
 
     let [editMode, setEditMode] = useState(false);
+    let [title, setTitle] = useState('')
 
-    let activateEditMode = () => setEditMode(true);
+    let activateEditMode = () => {
+        setEditMode(true)
+        setTitle(props.title)
+    };
     let activateViewMode = () => setEditMode(false);
+    let onChangeTitleHandler = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)
 
     return editMode
-        ? <input value={props.title} onBlur={activateViewMode}/>
+        ? <input value={title} onBlur={activateViewMode} onChange={onChangeTitleHandler} autoFocus/>
         :<span onDoubleClick={activateEditMode}>{props.title}</span>
 }
