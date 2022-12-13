b4c14a984b5380bc3ebff70d0c7c5c659f6094b5
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-12-13 17:34:47 +0100

    REMOVE-TASK

--- my-app/src/state/tasks-reducer.ts
+++ my-app/src/state/tasks-reducer.ts
@@ -1,22 +1,27 @@
 import {TasksStateType} from '../App';
 
 
-export type Action1Type = {
-    type: '1',
-    id: string
+export type RemoveTaskActionType = {
+    type: 'REMOVE-TASK',
+    taskId: string
+    todolistId: string
 }
 export type Action2Type = {
     type: '2',
     title: string
 }
 
-type ActionsType = Action1Type | Action2Type
+type ActionsType = RemoveTaskActionType | Action2Type
 
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
     switch (action.type) {
-        case "1": {
-            return {...state}
+        case 'REMOVE-TASK': {
+            const stateCopy = {...state};
+            const tasks = state[action.todolistId];
+            const filteredTasks = tasks.filter(t => t.id !== action.taskId)
+            stateCopy[action.todolistId] = filteredTasks;
+            return stateCopy
         }
         case "2": {
             return {...state}
@@ -26,8 +31,8 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksS
     }
 }
 
-export const action1AC = (todolistId: string): Action1Type => {
-    return { type: '1', id: todolistId}
+export const removeTaskAC = (taskId: string ,todolistId: string): RemoveTaskActionType => {
+    return { type: 'REMOVE-TASK', todolistId, taskId}
 }
 export const action2AC = (title: string): Action2Type => {
     return { type: '2', title: title}
