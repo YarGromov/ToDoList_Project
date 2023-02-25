e672b3173e21102828a7798d4c89a43fb3ff3eb7
YarGromov <1l1l1arr1l1l1@gmail.com>
2023-02-25 16:53:29 +0100

    EditableSpan.stories.tsx

---
+++ my-app/src/EditableSpan.stories.tsx
@@ -0,0 +1,15 @@
+
+import {EditableSpan} from "./EditableSpan";
+import {action} from "@storybook/addon-actions";
+
+
+export default {
+    title: 'EditableSpan Component',
+    component: EditableSpan
+}
+
+const changeCallback = action("Value changed")
+
+export const EditableSpanBaseExample = () => {
+    return <EditableSpan value={"Start Value"} onChange={changeCallback}/>
+}
