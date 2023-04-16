f10763618f8215170c372bde48fc32445bd6684d
YarGromov <1l1l1arr1l1l1@gmail.com>
2023-04-16 15:50:07 +0200

    Instanse

--- my-app/src/api/todolist-api.ts
+++ my-app/src/api/todolist-api.ts
@@ -9,40 +9,38 @@ const settings = {
     },
 }
 
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    withCredentials: true,
+    headers: {
+        'API-KEY': '9c4ffa8f-2b09-41e8-a62a-7e4b43a79ee7',
+    },
+})
+
+
 export const todolistAPI = {
 
     getTodolists() {
-        const promise = axios.get(
-            'https://social-network.samuraijs.com/api/1.1/todo-lists',
-            settings
-        )
-        return promise
+        return  instance.get('todo-lists')
     },
 
     createTodolist(title: string) {
-        const promise = axios.post(
-            'https://social-network.samuraijs.com/api/1.1/todo-lists',
-            {title: title},
-            settings
+        return instance.post(
+            'todo-lists',
+            {title: title}
         )
-        return promise
     },
 
     deleteTodolist(todolistId: string) {
-        const promise = axios.delete(
-            `https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`,
-            settings
+        return instance.delete(
+            `/todo-lists/${todolistId}`
         )
-        return promise
     },
 
     updateTodolist(todolistId: string, title: string) {
-        const promise = axios.put(
-            `https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`,
-            {title: title},
-            settings
+        return instance.put(
+            `todo-lists/${todolistId}`,
+            {title: title}
         )
-        return promise
-    },
-
+    }
 }
--- my-app/src/stories/todolists-api.stories.tsx
+++ my-app/src/stories/todolists-api.stories.tsx
@@ -1,18 +1,9 @@
 import React, {useEffect, useState} from 'react'
-import axios from "axios";
 import {todolistAPI} from "../api/todolist-api";
 
 export default {
     title: 'API'
 }
-
-const settings = {
-    withCredentials: true,
-    headers: {
-        'API-KEY': '9c4ffa8f-2b09-41e8-a62a-7e4b43a79ee7'
-    }
-}
-
 
 export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
@@ -26,7 +17,7 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        const title = 'REDUX'
+        const title = 'NEST_JS'
         todolistAPI.createTodolist(title)
             .then((res) => {
                 setState(res.data)
@@ -39,7 +30,7 @@ export const CreateTodolist = () => {
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        const todolistId = '160d4dfa-cca2-45b1-a31a-ff7d1a253bda'
+        const todolistId = '9db758c7-b443-42e0-bdb0-f7a7d74dc972'
         todolistAPI.deleteTodolist(todolistId)
             .then((res) => {
                 setState(res.data)
@@ -52,8 +43,8 @@ export const DeleteTodolist = () => {
 export const UpdateTodolistTitle = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        const todolistId = '5cb6e298-cb62-4d63-9023-2fb8914ce1b1'
-        const title = 'SOME NEW TITLE!!!!'
+        const todolistId = '3be05336-e455-4564-b18b-17aebb0da783'
+        const title = 'UPDATE TITLE'
         todolistAPI.updateTodolist(todolistId, title).then((res) => {
             setState(res.data)
         })
